5ff27f3f69921b8c00e18a6d41b06468e7bb14b3
barrioscrespomatias <matisfd@gmail.com>
2023-09-30 00:21:48 -0300

    App 01 splash e icono

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,32 +1,28 @@
-  /// <reference types="@capacitor/splash-screen" />
+import { CapacitorConfig } from '@capacitor/cli';
 
-  import { CapacitorConfig } from '@capacitor/cli';
-
-  const config: CapacitorConfig = {
-    appId: 'com.example.app',
-    appName: 'primeraApp',
-    webDir: 'www',
-    server: {
-      androidScheme: 'https'
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'app01',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
     },
-    plugins: {
-      SplashScreen: {
-        launchShowDuration: 3000,
-        launchAutoHide: true,
-        launchFadeOutDuration: 3000,
-        backgroundColor: "#ffffffff",
-        androidSplashResourceName: "splash",
-        androidScaleType: "CENTER_CROP",
-        showSpinner: true,
-        androidSpinnerStyle: "large",
-        iosSpinnerStyle: "small",
-        spinnerColor: "#999999",
-        splashFullScreen: true,
-        splashImmersive: true,
-        layoutName: "launch_screen",
-        useDialog: true,
-      },
-    }
-  };
-  
-  export default config;
+  }
+};
